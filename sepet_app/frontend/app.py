5363d4156b6c1e94a6b00cfb63c768dccf9296db
Bar-Kal <baris.kalayci@protonmail.com>
2025-12-08 23:49:27 +0100

    host added

--- sepet_app/frontend/app.py
+++ sepet_app/frontend/app.py
@@ -34,4 +34,4 @@ app = create_app()
 if __name__ == '__main__':
     # The debug=True flag enables the interactive debugger and auto-reloader.
     # This should only be used for development.
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
