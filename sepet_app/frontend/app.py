51a80a93eb0daf4cde07685f21c3f7c0b07a6e4e
Bar-Kal <baris.kalayci@protonmail.com>
2025-12-19 14:06:55 +0100

    404 page added

--- sepet_app/frontend/app.py
+++ sepet_app/frontend/app.py
@@ -26,6 +26,12 @@ def create_app():
 # Create the application instance using the factory function
 app = create_app()
 
+@app.errorhandler(404)
+def not_found_error(error):
+    # Render the custom 404 page and return it with a 404 status code
+    from flask import render_template
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     # The debug=True flag enables the interactive debugger and auto-reloader.
     # This should only be used for development.
