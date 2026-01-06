8c26f871289afbcc89088193c0197695d0ba8676
Bar-Kal <baris.kalayci@protonmail.com>
2026-01-06 13:37:59 +0100

    print added

--- sepet_app/uploader/app.py
+++ sepet_app/uploader/app.py
@@ -24,6 +24,7 @@ def create_app():
 
     @app.route('/', methods=['GET', 'POST'])
     def upload_file():
+        print("Started to upload file...")
         message = None
         category = None
         if request.method == 'POST':
